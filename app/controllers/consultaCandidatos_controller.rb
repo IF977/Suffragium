543841f90822e08e293e504a52f0db1f27416877
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-07 01:02:38 +0000

    Improvement

--- app/controllers/consultaCandidatos_controller.rb
+++ app/controllers/consultaCandidatos_controller.rb
@@ -1,4 +1,4 @@
-class consultaCandidatosController < ApplicationController
+class ConsultaCandidatosController < ApplicationController
 
   def consultaCandidatos                                              
     @consultaCandidatos = Candidates.all
@@ -6,5 +6,25 @@ class consultaCandidatosController < ApplicationController
   
   def candidate_params
         params.require(:candidate).permit(:nome, :partido, :numero)
+  end
+  def new
+    # default: render ’new’ template
+  end
+  def create
+    @candidate = candidate.create!(params[:candidate])
+    flash[:notice] = "#{@candidates.nome} was successfully created."
+    redirect_to candidate_path
+  end
+  def update
+    @candidate = candidate.find params[:id]
+    @candidate.update_attributes!(params[:candidate])
+    flash[:notice] = "#{@candidate.nome} was successfully updated."
+    redirect_to candidate_path(@candidate)
+  end
+  def destroy
+    @candidate = candidate.find(params[:id])
+    @candidate.destroy
+    flash[:notice] = "Candidate ’#{@candidate.nome}’ deleted."
+    redirect_to candidate_path
   end
 end
