43894b0c53e923fd67ababa39c630050e029f698
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-07 23:04:23 +0000

    heroku

--- app/controllers/ConsultaCandidatos_controller.rb
+++ app/controllers/ConsultaCandidatos_controller.rb
@@ -1,10 +1,30 @@
 class ConsultaCandidatosController < ApplicationController
 
-  def ConsultaCandidatos                                              
-    @ConsultaCandidatos = Candidates.all
+  def consultaCandidatos                                              
+    @consultaCandidatos = Candidates.all
   end
   
   def candidate_params
         params.require(:candidate).permit(:nome, :partido, :numero)
+  end
+  def new
+    # default: render ’new’ template
+  end
+  def create
+    @candidate = candidate.create!(params[:candidate])
+    flash[:notice] = "#{@candidates.nome} was successfully created."
+    redirect_to candidate_path
+  end
+  def update
+    @candidate = candidate.find params[:id]
+    @candidate.update_attributes!(params[:candidate])
+    flash[:notice] = "#{@candidate.nome} was successfully updated."
+    redirect_to candidate_path(@candidate)
+  end
+  def destroy
+    @candidate = candidate.find(params[:id])
+    @candidate.destroy
+    flash[:notice] = "Candidate ’#{@candidate.nome}’ deleted."
+    redirect_to candidate_path
   end
 end
