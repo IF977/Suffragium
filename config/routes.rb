7e8f5c4fed8f0911c4d33e655812eb000889ee8b
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-09 00:57:43 +0000

    Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  
   resources :candidatos
-  root 'teste#index'
+  root 'candidatos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
