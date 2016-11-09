5667a2ded52448bcad5ba6efe5dfb6d44c9a44f5
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-09 12:30:18 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   
 resources :user_sessions
 resources :users
-get 'login' => 'user_sessions#new', :as => :login
-post 'logout' => 'user_sessions#destroy', :as => :logout
+#get 'login' => 'user_sessions#new', :as => :login
+#post 'logout' => 'user_sessions#destroy', :as => :logout
+devise_for :users, skip: [:sessions]
+as :user do
+  get 'signin', to: 'devise/sessions#new', as: :new_user_session
+  post 'signin', to: 'devise/sessions#create', as: :user_session
+  match 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+end
 end
