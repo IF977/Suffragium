64283f5b6767da57973830e229186e51f69bf69c
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-09 21:45:31 +0000

    Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :candidatos
   get 'users/new'
 
@@ -8,4 +10,14 @@ resources :user_sessions
 resources :users
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+end
 end
