6805903f7d3ce52a28df19b4ab01d6d7db221350
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-09 00:26:46 +0000

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :casas
+  root 'casas#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
