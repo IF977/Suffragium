3378b3ce5535a8d9c4ce13d2e06ae7fff12b3bbd
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-09 01:56:40 +0000

    heroku

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,74 +1,20 @@
 class UserSessionsController < ApplicationController
-  before_action :set_user_session, only: [:show, :edit, :update, :destroy]
-
-  # GET /user_sessions
-  # GET /user_sessions.json
-  def index
-    @user_sessions = UserSession.all
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.json
-  def show
-  end
-
-  # GET /user_sessions/new
+  skip_before_action :require_login, except: [:destroy]
   def new
-    @user_session = UserSession.new
+    @user = User.new
   end
 
-  # GET /user_sessions/1/edit
-  def edit
-  end
-
-  # POST /user_sessions
-  # POST /user_sessions.json
   def create
-    @user_session = UserSession.new(user_session_params)
-
-    respond_to do |format|
-      if @user_session.save
-        format.html { redirect_to @user_session, notice: 'User session was successfully created.' }
-        format.json { render :show, status: :created, location: @user_session }
-      else
-        format.html { render :new }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
-      end
+    if @user = login(params[:login], params[:password])
+      redirect_back_or_to(:users, notice: 'Login realizado com sucesso!')
+    else
+      flash.now[:alert] = 'Falha no Login!'
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /user_sessions/1
-  # PATCH/PUT /user_sessions/1.json
-  def update
-    respond_to do |format|
-      if @user_session.update(user_session_params)
-        format.html { redirect_to @user_session, notice: 'User session was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user_session }
-      else
-        format.html { render :edit }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.json
   def destroy
-    @user_session.destroy
-    respond_to do |format|
-      format.html { redirect_to user_sessions_url, notice: 'User session was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    logout
+    redirect_to(:users, notice: 'Logged out!')
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user_session
-      @user_session = UserSession.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_session_params
-      params.fetch(:user_session, {})
-    end
 end
